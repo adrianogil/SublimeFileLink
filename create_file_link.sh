de8c7b18eea450c770e4743ab10afb7c694ef33e
Adriano Gil <adrianogil.san@gmail.com>
2018-03-17 07:14:36 -0400

    Added quotes to fix bug with spaced names

--- create_file_link.sh
+++ create_file_link.sh
@@ -6,7 +6,7 @@ sha1_file_path=~/filelinks/$sha1_file_name'.sh'
 
 echo '# File Link'                          > $sha1_file_path
 echo ''                                    >> $sha1_file_path
-echo 'sublime '$file_name':'$line_in_file  >> $sha1_file_path
+echo 'sublime "'$file_name'":'$line_in_file  >> $sha1_file_path
 
 chmod +x $sha1_file_path
 
