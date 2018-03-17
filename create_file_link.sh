741645c619951bbc8072365cd3685a75d914689e
Adriano Gil <adrianogil.san@gmail.com>
2018-03-17 07:17:41 -0400

    Verify if filename is note then generate filelinks in Notes folder

--- create_file_link.sh
+++ create_file_link.sh
@@ -2,7 +2,14 @@ file_name=$1
 line_in_file=$2
 
 sha1_file_name=$(echo -n $file_name":"$line_in_file | openssl sha1 | cut -c10-)
-sha1_file_path=~/filelinks/$sha1_file_name'.sh'
+
+if [ ${file_name: -5} == ".note" ]
+then
+    sha1_file_path=~/Notes/filelinks/$sha1_file_name'.sh'
+else
+    sha1_file_path=~/filelinks/$sha1_file_name'.sh'
+fi
+
 
 echo '# File Link'                          > $sha1_file_path
 echo ''                                    >> $sha1_file_path
